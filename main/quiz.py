67225d617f2c1b2bc91f258545c74646342236ff
TaffetaEarth <yasha10122@gmail.com>
2021-10-18 11:20:21 +0300

    quiz update

--- main/quiz.py
+++ main/quiz.py
@@ -1,6 +1,11 @@
 import sqlite3
 
+
+def level(level):
+    cursor.execute("")
+
+
+r_answ = 0
 connection = sqlite3.connect("quizdb.sqlite3")
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE Quiz (id INTEGER, question TEXT, answers TEXT, "
-               "correct_answer INTEGER);")
+
